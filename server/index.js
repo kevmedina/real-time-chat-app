0d2b28a4b66946f8ae35aba4b410aae39f0b90ca
Kevin Medina <kmedinakm@hotmail.com>
2020-06-24 21:52:06 -0400

    working on sending messages from client to client

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
 const PORT = process.env.PORT || 5000;
 const router = require("./router");
@@ -11,9 +12,27 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on("connection", (socket) => {
-  console.log("We have a connection!");
-  socket.on("join", ({ name, room }) => {
-    console.log(name, room);
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.emit("message", { text: `${user.name}, welcome to the room!` });
+    socket.broadcast.to(user.room).emit("message", {
+      user: "admin",
+      text: `${user.name}, has joined!`,
+    });
+
+    socket.join(user.name);
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    callback();
   });
 
   socket.on("disconnect", () => {
